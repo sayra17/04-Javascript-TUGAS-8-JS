ab4c957e4663b18bd540c9b35a69898ead7d83e8
sayra17 <agussayra03@gmail.com>
2022-02-03 17:47:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+04-Javascript-TUGAS-8-JS
---
+++ tugas8.js
@@ -0,0 +1,12 @@
+function data() {
+    var sepatu = {
+        brand: "NIKE",
+        harga: 2000,
+        warna: "putih",
+    }
+    sepatu.tahun = 2012;
+    sepatu.harga = 2000000;
+    console.log(sepatu)
+    console.log(sepatu.harga)
+}
+data()
